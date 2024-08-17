7abcbdfdb6b3209e84d55cc87e8b9a4f15ce4dd7
Paul Lee <contact@paullee.dev>
2024-08-16 22:28:00 -0400

    add react native paper

--- src/app/main/home.tsx
+++ src/app/main/home.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log('Pressed')}
+      >
+        Press me
+      </Button>
     </View>
   );
 }
--- src/app/root.tsx
+++ src/app/root.tsx
@@ -2,11 +2,51 @@ import { registerRootComponent } from 'expo';
 import 'react-native-gesture-handler';
 import { MainLayout } from './main/layout';
 import { NavigationContainer } from '@react-navigation/native';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  PaperProvider,
+  adaptNavigationTheme,
+  MD3LightTheme,
+  MD3DarkTheme,
+} from 'react-native-paper';
+import { useColorScheme } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = {
+  ...MD3LightTheme,
+  ...LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    ...LightTheme.colors,
+  },
+};
+const CombinedDarkTheme = {
+  ...MD3DarkTheme,
+  ...DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    ...DarkTheme.colors,
+  },
+};
 
 function RootLayout() {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
-    <NavigationContainer>
-      <MainLayout />
+    <NavigationContainer theme={theme}>
+      <PaperProvider theme={theme}>
+        <StatusBar style="auto" />
+        <MainLayout />
+      </PaperProvider>
     </NavigationContainer>
   );
 }
