f51a3716d935343055a22a02cfd8c560573adcfc
Paul Lee <contact@paullee.dev>
2024-09-05 12:04:35 -0400

    handle platform differences

--- src/lib/query/image.ts
+++ src/lib/query/image.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { post } from "../utils/request";
 import { ImageUploadResponse } from "@/lib/schema/image";
+import { Platform } from "react-native";
 
 interface ImageUploadProps {
   uri: string;
@@ -12,12 +13,16 @@ export function useImageUpload() {
   return useMutation({
     mutationFn: async ({ uri, mime, filename }: ImageUploadProps) => {
       let formData = new FormData();
-      // @ts-expect-error: React Native's fetch differs from web's fetch
-      formData.append("imageUpload", {
-        uri: uri,
-        type: mime,
-        name: filename ?? "blob",
-      });
+      if (Platform.OS == "web") {
+        formData.append("imageUpload", await (await fetch(uri)).blob());
+      } else {
+        // @ts-expect-error: React Native's fetch differs from web's fetch
+        formData.append("imageUpload", {
+          uri: uri,
+          type: mime,
+          name: filename ?? "blob",
+        });
+      }
 
       const response = await post("/image/upload", { form: formData });
       console.log("image key", ImageUploadResponse.parse(response).key);
